665f77d24db87869497cf2a09150169a3cfe6d1d
Sabbyasachi Roy <20575@ITCINFOTECH.COM>
2017-12-06 18:07:39 +0530

    routes and services integration

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
 const express = require('express');
-
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-const keys = require('./config/keys');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+authRoutes(app);
 
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
-        },(accessToken,refreshToken,profile,done)=>{
-            console.log('accessToken'+accessToken);
-            console.log("profile  Name "+profile.displayName);
-        }
-    )
-);
-app.get('/auth/google',
-    passport.authenticate('google',{
-        scope:['profile','email']
-    })
-)
-
-app.get('/auth/google/callback',passport.authenticate('google'));
-// app.get('/',(req,res)=>{
-//     res.send({'goodnight':'Momo'});
-// });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
