9510b2e9a5026e33ab7ec1c53c848e9ef3ccc175
Sabbyasachi Roy <20575@ITCINFOTECH.COM>
2017-12-07 20:03:19 +0530

    Authentication Complete

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+const keys = require('./config/keys');
+require('./model/users');
 require('./services/passport');
 const authRoutes = require('./routes/authRoutes');
 
-const app = express();
-authRoutes(app);
+mongoose.connect(keys.mongoURI);
 
+const app = express();
+app.use(
+    cookieSession({
+        maxAge:30*24*60*60*1000,
+        keys : [keys.cookieKey]
+    })
+)
+app.use(passport.initialize());
+app.use(passport.session())
+
+authRoutes(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,7 +7,13 @@ module.exports = (app)=>{
     )
 
     app.get('/auth/google/callback',passport.authenticate('google'));
-    // app.get('/',(req,res)=>{
-    //     res.send({'goodnight':'Momo'});
-    // });
+
+    app.get('/api/current_user',(req,res)=>{
+        res.send(req.user);
+    });
+
+    app.get('/api/logout',(req,res)=>{
+        req.logout();
+        res.send(req.user);
+    });
 }
